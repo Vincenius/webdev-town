a41ec8f2edae9d5da0c1a2a5fb72ff65a038049c
Vincenius <vincent.will@vaitrade.de>
2023-08-04 09:08:00 +0200

    add fallback for missing query parameter

--- lambda/index.js
+++ lambda/index.js
@@ -2,8 +2,8 @@
 const db = require('./database')
 
 exports.handler = async (event) => {
-  const search = event["queryStringParameters"]["q"]
-  const pageString = event["queryStringParameters"]["page"]
+  const queryParams = event["queryStringParameters"] || {}
+  const { q: search, page: pageString } = queryParams;
 
   const page = parseInt(pageString, 10) || 1;
   const { result, count } = await db.getByQuery({ search, page });
