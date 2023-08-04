ad5eb75e0478faa68f4fe0ba662723d9ebeefd16
Vincenius <vincent.will@vaitrade.de>
2023-08-04 08:47:20 +0200

    init first version of lambda api

--- lambda/index.js
+++ lambda/index.js
@@ -1,12 +1,20 @@
 'use strict'
+const db = require('./database')
 
-export const handler = async (event) => {
-  // TODO implement
+exports.handler = async (event) => {
+  const search = event["queryStringParameters"]["q"]
+  const pageString = event["queryStringParameters"]["page"]
+
+  const page = parseInt(pageString, 10) || 1;
+  const { result, count } = await db.getByQuery({ search, page });
+
   const response = {
     statusCode: 200,
-    body: JSON.stringify('DEPLOY TEST NEW!'),
+    body: JSON.stringify({
+      page,
+      total: count,
+      result,
+    }),
   };
   return response;
 };
-
-
