a8fcaafe677a78b00573e147cbf09a31bb1db339
Vincenius <vincent.will@vaitrade.de>
2023-10-11 14:08:09 +0200

    fix linting

--- frontend/app/thank-you/page.js
+++ frontend/app/thank-you/page.js
@@ -6,11 +6,11 @@ const Thanks = () => {
     <h1>Thank you for subscribing!</h1>
 
     <h3>📅 What to Expect:</h3>
-    <p>Every <b>Wednesday morning</b>, I'll send you an e-mail with the most valuable web development resources right to your inbox.</p>
+    <p>Every <b>Wednesday morning</b>, I&apos;ll send you an e-mail with the most valuable web development resources right to your inbox.</p>
 
-    <h3>📚 Check Out What's Already Here:</h3>
+    <h3>📚 Check Out What&apos;s Already Here:</h3>
     <p>
-      Can't wait for the next issue? You can find all previous resources and some useful collections on the website:
+      Can&apos;t wait for the next issue? You can find all previous resources and some useful collections on the website:
     </p>
     <p>
       <Link href="/">🔗 All resources from previous issues</Link><br/>
